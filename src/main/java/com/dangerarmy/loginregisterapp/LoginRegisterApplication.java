e244f95f6bb325f6d741ad3279c3c63ce3cd4042
Vishal Bhagwan Gadge <vishalgadge2005@gmail.com>
2026-07-10 20:52:21 +0530

    moved important env variables to .env file and added dependency and changed main method

--- src/main/java/com/dangerarmy/loginregisterapp/LoginRegisterApplication.java
+++ src/main/java/com/dangerarmy/loginregisterapp/LoginRegisterApplication.java
@@ -1,5 +1,6 @@
 package com.dangerarmy.loginregisterapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +10,8 @@ import org.springframework.cache.annotation.EnableCaching;
 public class LoginRegisterApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(LoginRegisterApplication.class, args);
     }
 }
